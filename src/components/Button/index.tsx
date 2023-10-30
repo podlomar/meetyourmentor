ec86522adf73a4989322813cf8e083503997c1c5
Eva Machova <evca.machova@gmail.com>
2023-10-30 23:10:40 +0100

    přidat loading spinner

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,14 @@
+'use client';
+
 import { ReactNode } from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.scss';
 import Link from 'next/link';
+// @ts-expect-error
+// NOTE: experimental_useFormStatus is not in the types and useFormStatus would require Next.JS version upgrade.
+// SEE: https://github.com/vercel/next.js/issues/49232#issuecomment-1775419298
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import LoadingSpinner from 'components/LoadingSpinner';
 
 interface Props {
   onClick?: () => void;
@@ -13,9 +20,10 @@ interface Props {
 
 const Button = (
   { onClick, href, primary = false, disabled = false, children
-}: Props): JSX.Element => {
-  const className = clsx(styles.btn, primary && styles.primary);  
-  
+  }: Props): JSX.Element => {
+  const className = clsx(styles.btn, primary && styles.primary);
+  const { pending } = useFormStatus();
+
   if (href !== undefined) {
     return (
       <Link className={className} href={href}>
@@ -25,7 +33,8 @@ const Button = (
   }
 
   return (
-    <button disabled={disabled} className={className} onClick={onClick}>
+    <button disabled={disabled || pending} className={className} onClick={onClick}>
+      {pending && <LoadingSpinner />}
       {children}
     </button>
   );
---
+++ src/components/LoadingSpinner/index.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import styles from './styles.module.scss';
+
+const LoadingSpinner = (): JSX.Element => (
+  <div className={styles.spinner}></div>
+);
+
+export default LoadingSpinner;
